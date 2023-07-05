15d8256d13e1a918f6220101b733a3dea8ca95d4
konsulin-developer <team@konsulinaja.id>
2023-07-05 09:39:02 +0700

    main pertemuan 3

---
+++ lib/pertemuan3/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+import 'package:my_first_project/pertemuan3/feed_page.dart';
+import 'package:my_first_project/pertemuan3/simplefeed_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SimpleFeedPage(),
+    );
+  }
+}
