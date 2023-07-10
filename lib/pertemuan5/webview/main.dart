c885e0d8ed14b049325a4cac63b32655ab957525
Thoriq-ha <team@konsulinaja.id>
2023-07-10 09:28:08 +0700

    pertemuan 5

---
+++ lib/pertemuan5/webview/main.dart
@@ -0,0 +1,19 @@
+// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: FeedPage(),
+//     );
+//   }
+// }
