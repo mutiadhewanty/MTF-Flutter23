dfeb073bfa9ac863e4f346c8efc5de58679c1222
Thoriq-ha <team@konsulinaja.id>
2023-07-10 12:19:58 +0700

    update materi sharedpref

--- lib/pertemuan5/shared_preferences/home_page.dart
+++ lib/pertemuan5/shared_preferences/home_page.dart
@@ -1,11 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:my_first_project/pertemuan5/shared_preferences/shared_pref.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var baseUrl = "http://10.0.2.2:8000/api";
+  String tokenTanpaSharedpref = "";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Future<void> login() async {
+      var dio = Dio();
+      var response = await dio.post(
+        "${baseUrl}/login",
+        data: {"email": "marlin09@example.com", "password": "password"},
+      );
+      SharedPref.pref?.setString("token", response.data["data"]["token"]);
+      tokenTanpaSharedpref = response.data["data"]["token"].toString();
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("${tokenTanpaSharedpref}"),
+            Text("${SharedPref.pref!.getString("token")}"),
+            ElevatedButton(
+                onPressed: () {
+                  login();
+                  setState(() {});
+                },
+                child: Text("Login"))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pertemuan5/shared_preferences/main.dart
+++ lib/pertemuan5/shared_preferences/main.dart
@@ -1,19 +1,23 @@
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:my_first_project/pertemuan5/shared_preferences/home_page.dart';
+import 'package:my_first_project/pertemuan5/shared_preferences/shared_pref.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPref.init();
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: FeedPage(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
