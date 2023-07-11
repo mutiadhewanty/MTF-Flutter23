516524c2e1e3203731509f4ccb528fe2ba4d87ec
Thoriq-ha <team@konsulinaja.id>
2023-07-11 10:12:06 +0700

    login slicing 1 step

---
+++ lib/pertemuan6/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
+          title: const Text("HomePage")),
+      body: const Center(child: Text("Ini Home page gaess!!")),
+    );
+  }
+}
---
+++ lib/pertemuan6/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+import 'package:my_first_project/pertemuan6/login_page.dart';
+import 'package:my_first_project/pertemuan6/shared_pref.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await SharedPref.init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
