a4eec5acbff0b8349830d47087a422757691fac0
Thoriq-ha <team@konsulinaja.id>
2023-07-12 11:31:48 +0700

    update pertemuan 7

---
+++ test/object_test.dart
@@ -0,0 +1,7 @@
+import 'package:my_first_project/pertemuan7/model/post.dart';
+import 'package:my_first_project/pertemuan7/model/user.dart';
+
+void main() {
+  Post post =
+      Post(picture: "", content: "", title: "", user: User.fromJson({}));
+}
